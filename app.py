f4c6edc816473eb65e68a160a8d246d0efa4543b
Somi <somii009@ewhain.net>
2021-08-22 21:34:02 +0900

    Add api

--- app.py
+++ app.py
@@ -4,11 +4,26 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api', methods=['GET'])
 def detect():
+    url = request.args.get('url')
 
     res = classify()
     return res
+
+
+@app.route('/api/phishing/register', methods=['GET'])
+def register():
+    url = request.args.get('url')
+
+
+# @app.route('/api/phishing/database', methods=['GET'])
+# url 불러오기
+
+
+# @app.route('/api/phishing/count', methods=['GET'])
+#url 개수 반환
+
 
 
 if __name__ == '__main__':
