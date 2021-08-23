4bc21e68494ae6cddffaf5778f8555b34f7bd7db
Park Hanwool <nanometre380@gmail.com>
2021-08-24 03:12:20 +0900

    add api related database

--- models.py
+++ models.py
@@ -5,13 +5,18 @@ class Phishing(db.Model) :
 
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String())
-    description = db.Column(db.String())
-    timestamp = db.Column(db.DateTime())
+    timestamp = db.Column(db.DateTime, default = db.func.current_timestamp())
 
-    def __init__(self, url, description, datetime) :
+    def __init__(self, url) :
         self.url = url
-        self.description = description
-        self.timestamp = timestamp
+
+    def save(self) :
+        db.session.add(self)
+        db.session.commit()
+
+    @staticmethod
+    def get_all() :
+        return Phishing.query.all()
 
     def __repr__(self) :
         return f"<ID {self.id}  , URL {self.url}>"
